89aab01bd1c9d39c7a6c56040afabcd90cf435f9
Tim Konieczny <timkonieczny@gmail.com>
2017-06-15 14:22:45 +0200

    Added color scale

--- parser.js
+++ parser.js
@@ -1,13 +1,18 @@
 $(document).ready(function (){
+    var colorScale = chroma.scale("Spectral").domain([1,7]);
     $.get("data/data.json", function (data) {
+        console.log(data);
         d3.select("body").selectAll("div")
             .data(data)
             .enter()
             .append("div")
             .attr("class", "bar")
+            .style("background-color", function(d) {
+                return colorScale(d["car-type"]);
+            })
             .style("height", function(d) {
-                // console.log(d["car-type"]);
-                var barHeight = d["car-type"] * 5;
+                //var barHeight = d["car-type"] * 5;
+                var barHeight = 40;
                 return barHeight + "px";
             });
     });
